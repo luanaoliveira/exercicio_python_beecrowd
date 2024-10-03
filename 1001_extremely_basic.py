4440aa1ac8f065fa496e5b4b5ceac9b8a2c6282f
Luana Oliveira <luanasilva.jua@gmail.com>
2024-10-03 12:23:48 -0300

    Add exercise 1001_extremely_basic

---
+++ 1001_extremely_basic.py
@@ -0,0 +1,8 @@
+# Soma de dois números
+
+number1 = int(input()) # Recebe o primeiro número
+number2 = int(input()) # Recebe o segundo número
+
+sum = number1 + number2 # Faz a operação de soma 
+
+print("X =", sum) # Imprime o resultado da soma
