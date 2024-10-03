57a1f6bddee3332efaf5c7ca95f7f197aa6d010a
Luana Oliveira <luanasilva.jua@gmail.com>
2024-10-03 12:40:51 -0300

    Add exercise 1002_area_of_a _circle

---
+++ 1002_area_of_a _circle.py
@@ -0,0 +1,10 @@
+# Área do círculo
+
+radius = float(input())  # Recebe o valor do raio
+
+pi = 3.14159 # Valor da constante pi
+
+area = pi * radius * radius # Cálculo da área do círculo
+
+print(f"A={area:.4f}") #Imprime o resultado
+
