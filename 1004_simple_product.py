59fed8b3fad30f1a086d27eadce0715177845e3c
Luana Oliveira <luanasilva.jua@gmail.com>
2024-10-05 15:56:00 -0300

    Add exercise 1004_simple_product

---
+++ 1004_simple_product.py
@@ -0,0 +1,8 @@
+# Produto de dois números
+
+number1 = int(input()) # Recebe o primeiro número
+number2 = int(input()) # Recebe o segundo número
+
+product = number1 * number2 # Calcula o produto entre os dois números
+
+print("PROD =", product) # Imprime o resulatdo do produto
