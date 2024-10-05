7773a76eacca3f0270d05989c6c7ec390ab0632b
Luana Oliveira <luanasilva.jua@gmail.com>
2024-10-05 15:46:04 -0300

    Add exercise 1003_simple_sum

---
+++ 1003_simple_sum.py
@@ -0,0 +1,8 @@
+# Soma de dois números
+
+number1 = int(input()) # Recebe o primeiro número
+number2 = int(input()) # Recebe o segundo número
+
+sum = number1 + number2 # Calcula a soma dos dois números
+
+print("SOMA =", sum) # Imprime o resultado da soma
