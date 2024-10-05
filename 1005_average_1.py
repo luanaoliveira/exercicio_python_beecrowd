22192e2f5879768721257ce22886bb611627abb9
Luana Oliveira <luanasilva.jua@gmail.com>
2024-10-05 16:37:09 -0300

    Add exercise 1005_average_1

---
+++ 1005_average_1.py
@@ -0,0 +1,9 @@
+# Média de notas
+
+grade1 = float(input()) # Recebe a primeira grade
+grade2 = float(input()) # Recebe a segunda nota
+
+average = ((grade1 * 3.5) + (grade2 * 7.5)) / (3.5 + 7.5) # Calcula a média
+
+print(f"MEDIA = {average:.5f}") # Imprime a média
+
