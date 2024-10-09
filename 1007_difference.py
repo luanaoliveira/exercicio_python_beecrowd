77c10b1319c4146382197199e827ce38f54767c7
Luana Oliveira <luanasilva.jua@gmail.com>
2024-10-09 18:05:02 -0300

    Add exercise 1007_difference

---
+++ 1007_difference.py
@@ -0,0 +1,10 @@
+# Diferença entre os produtos
+
+number1 = int(input()) # Recebe o valor do primero número
+number2 = int(input()) # Recebe o valor do segundo número
+number3 = int(input()) # Recebe o valor do terceiro número
+number4 = int(input()) # Recebe o valor do quarto número
+
+difference = (number1 * number2) - (number3 * number4) # Calcula a difernça entre os produtos
+
+print(f"DIFERENCA = {difference}") # Imprime resultado da diferença 
